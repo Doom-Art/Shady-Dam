790b095d7e3c6f9272f6b17980fe0f4c1f8998d7
Doom-Art <ouginak121@gmail.com>
2022-11-09 09:38:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Shady_Dam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> map = new List<string>;
+            if (File.Exists(@"mapTest.txt"))
+            {
+                foreach(string i in File.ReadLines(@"mapTest.txt"))
+                {
+
+                }
+            }
+        }
+    }
+}
